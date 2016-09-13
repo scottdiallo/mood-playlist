0722aa8d1845ac1437f101af0207008eec71fb1b
scott_diallo <diallot@me.com>
2016-09-13 19:12:08 +0000

    fixing spotify api

--- server.js
+++ server.js
@@ -1,32 +1,49 @@
-var unirest = require("unirest");
-var express = require('express');
-var events = require('events');
+// var unirest = require('unirest');
+// var express = require('express');
+// var events = require('events');
+// var app = express();
+// app.use(express.static('public'));
 
-var app = express();
+// //first API call to get the artist ID by name search
+// var getFromApi = function(endpoint, args) {
+//     var emitter = new events.EventEmitter();
+//     unirest.get('https://api.spotify.com/v1/' + endpoint)
+//         .qs(args)
+//         .end(function(response) {
+//             console.log('hello');
+//             if (response.ok) {
+//                 emitter.emit('end', response.body);
+//             }
+//             else {
+//                 emitter.emit('error', response.code);
+//             }
+//         });
+//     return emitter;
+// };
 
-//making api call to spotify
-var getFromApi = function(endpoint, args){
-    var emitter = new events.EventEmitter();
-    unirest.get('https://api.spotify.com/v1/' + endpoint)
-    .qs(args)
-    .end(function(response){
-        if(response.ok){
-            emitter.emit('end', response.body)
-        }
-        else {
-            emitter.emit('error', response.code);
-        }
-    });
-    return emitter;
-};
+// app.get('/artists/:name', function(req, res){
+//     var searchReq = getFromApi('search',{
+//         q: req.params.name,
+//         limit:10,
+//         type:'artist'
+//     });
+// })
 
-app.get('/search/:name', function(req, res){
-    var searchReq = getFromApi('search',{
-        q: req.params.name,
-        limit:10,
-        type:'artist'
-    });
-})
+// $(document).ready(function(){
+//     $('.generateButton').click(function(){
+//         var resultElement = $('#resultDiv');
+//         var userSelection = $('formGroup').val();
+//         $.ajax({
+//             url: 'https://api.spotify.com/v1/users/{user_id}/playlists',
+//             method: 'get',
+//             data: { q: userSelection },
+//             dataType: 'json',
+//             success: function(data){
+//                 console.log(data);
+//             }
+//         })
+//     })
+// })
 
-app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+// app.use(express.static('public'));
+// app.listen(process.env.PORT || 8080);
