f40a5ede8a5c1fe27e6b6ffe8ee6440701d412a3
Scott Diallo <diallot@me.com>
2016-09-13 12:13:59 -0700

    fixing spotify api
    minor

--- server.js
+++ server.js
@@ -21,6 +21,7 @@
 //     return emitter;
 // };
 
+<<<<<<< Updated upstream
 // app.get('/artists/:name', function(req, res){
 //     var searchReq = getFromApi('search',{
 //         q: req.params.name,
@@ -47,3 +48,30 @@
 
 // app.use(express.static('public'));
 // app.listen(process.env.PORT || 8080);
+=======
+//making api call to spotify
+var getFromApi = function (endpoint, args) {
+    var emitter = new events.EventEmitter();
+    unirest.get('https://api.spotify.com/v1/' + endpoint)
+        .qs(args)
+        .end(function (response) {
+            if (response.ok) {
+                emitter.emit('end', response.body)
+            } else {
+                emitter.emit('error', response.code);
+            }
+        });
+    return emitter;
+};
+
+app.get('/search/:name', function (req, res) {
+    var searchReq = getFromApi('search', {
+        q: req.params.name,
+        limit: 10,
+        type: 'artist'
+    });
+})
+
+app.use(express.static('public'));
+app.listen(process.env.PORT || 3000);
+>>>>>>> Stashed changes
