414c66173bd5c55fc731013c858554394302667c
scott_diallo <diallot@me.com>
2016-09-16 05:12:16 +0000

    changing api call from ajax to unirest

--- public/app.js
+++ public/app.js
@@ -5,42 +5,42 @@
 // };
 
 
+Spotify.prototype.onSearchSubmitted = function(event) {
+    event.preventDefault();
+    this.searchButton.trigger('click');
+};
+Spotify.prototype.search = function(name) {
+    var ajax = $.ajax('/search/' + name, {
+        type: 'GET',
+        dataType: 'json'
+    });
+    ajax.done(this.onSearchDone.bind(this));
+};
+Spotify.prototype.onSearchDone = function(artist) {
+    var result = $(this.artistTemplate(artist));
+    this.result.empty().append(result);
+    this.result.toggleClass('transparent');
+};
+$(document).ready(function() {
+    var app = new Spotify();
+});
 
-// Spotify.prototype.onSearchSubmitted = function(event) {
-//     event.preventDefault();
-//     this.searchButton.trigger('click');
-// };
-// Spotify.prototype.search = function(name) {
-//     var ajax = $.ajax('/search/' + name, {
-//         type: 'GET',
-//         dataType: 'json'
-//     });
-//     ajax.done(this.onSearchDone.bind(this));
-// };
-// Spotify.prototype.onSearchDone = function(artist) {
-//     var result = $(this.artistTemplate(artist));
-//     this.result.empty().append(result);
-//     this.result.toggleClass('transparent');
-// };
-// $(document).ready(function() {
-//     var app = new Spotify();
-// });
+//Ray mentor suggest not to use jquery for this.....?
+// $(document).ready(function(){
+//     $('.generateButton').click(function(){
+//         var resultElement = $('#resultDiv');
+//         var userSelection = $('formGroup').val();
+//         $.ajax({
+//             url: 'https://api.spotify.com/v1/users/{user_id}/playlists',
+//             method: 'get',
+//             data: { q: userSelection },
+//             dataType: 'json',
+//             success: function(data){
+//                 console.log(data);
+//             }
+//         })
+//     })
+// })
 
-$(document).ready(function(){
-    $('.generateButton').click(function(){
-        var resultElement = $('#resultDiv');
-        var userSelection = $('formGroup').val();
-        $.ajax({
-            url: 'https://api.spotify.com/v1/users/{user_id}/playlists',
-            method: 'get',
-            data: { q: userSelection },
-            dataType: 'json',
-            success: function(data){
-                console.log(data);
-            }
-        })
-    })
-})
-
-app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+// app.use(express.static('public'));
+// app.listen(process.env.PORT || 8080);
--- server.js
+++ server.js
@@ -1,77 +1,49 @@
-// var unirest = require('unirest');
-// var express = require('express');
-// var events = require('events');
-// var app = express();
-// app.use(express.static('public'));
+var unirest = require('unirest');
+var express = require('express');
+var events = require('events');
+var app = express();
+app.use(express.static('public'));
 
-// //first API call to get the artist ID by name search
-// var getFromApi = function(endpoint, args) {
-//     var emitter = new events.EventEmitter();
-//     unirest.get('https://api.spotify.com/v1/' + endpoint)
-//         .qs(args)
-//         .end(function(response) {
-//             console.log('hello');
-//             if (response.ok) {
-//                 emitter.emit('end', response.body);
-//             }
-//             else {
-//                 emitter.emit('error', response.code);
-//             }
-//         });
-//     return emitter;
-// };
-
-<<<<<<< Updated upstream
-// app.get('/artists/:name', function(req, res){
-//     var searchReq = getFromApi('search',{
-//         q: req.params.name,
-//         limit:10,
-//         type:'artist'
-//     });
-// })
-
-// $(document).ready(function(){
-//     $('.generateButton').click(function(){
-//         var resultElement = $('#resultDiv');
-//         var userSelection = $('formGroup').val();
-//         $.ajax({
-//             url: 'https://api.spotify.com/v1/users/{user_id}/playlists',
-//             method: 'get',
-//             data: { q: userSelection },
-//             dataType: 'json',
-//             success: function(data){
-//                 console.log(data);
-//             }
-//         })
-//     })
-// })
-
-// app.use(express.static('public'));
-// app.listen(process.env.PORT || 8080);
-=======
-//making api call to spotify
-var getFromApi = function (endpoint, args) {
+//first API call to get the artist ID by name search
+var getFromApi = function(endpoint, args) {
     var emitter = new events.EventEmitter();
     unirest.get('https://api.spotify.com/v1/' + endpoint)
         .qs(args)
-        .end(function (response) {
+        .end(function(response) {
+            console.log('hello');
             if (response.ok) {
-                emitter.emit('end', response.body)
-            } else {
+                emitter.emit('end', response.body);
+            }
+            else {
                 emitter.emit('error', response.code);
             }
         });
     return emitter;
 };
 
-app.get('/search/:name', function (req, res) {
-    var searchReq = getFromApi('search', {
+app.get('/artists/:name', function(req, res){
+    var searchReq = getFromApi('search',{
         q: req.params.name,
-        limit: 10,
-        type: 'artist'
+        limit:10,
+        type:'artist'
     });
 })
 
+$(document).ready(function(){
+    $('.generateButton').click(function(){
+        var resultElement = $('#resultDiv');
+        var userSelection = $('formGroup').val();
+        $.ajax({
+            url: 'https://api.spotify.com/v1/users/{user_id}/playlists',
+            method: 'get',
+            data: { q: userSelection },
+            dataType: 'json',
+            success: function(data){
+                console.log(data);
+            }
+        })
+    })
+})
+
 app.use(express.static('public'));
-app.listen(process.env.PORT || 3000);
->>>>>>> Stashed changes
+app.listen(process.env.PORT || 8080);
